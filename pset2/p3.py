9069c29c69506f542ca30e02a41722ac0bb287a6
Rafael Tartalia <rafael.tartalia@gmail.com>
2015-09-20 20:30:47 -0300

    fixed pset1-p3

--- pset2/p3.py
+++ pset2/p3.py
@@ -1,22 +1,22 @@
 #PROBLEM 3: USING BISECTION SEARCH TO MAKE THE PROGRAM FASTER
 
 #provided
-#balance = 999999
-#annualInterestRate = 0.18
+balance = 320000
+annualInterestRate = 0.2
 
 #local
 monthlyInterestRate = annualInterestRate / 12.0
-lowerBound = balance / 12
+lowerBound = balance / 12.0
 upperBound = (balance * (1 + monthlyInterestRate) ** 12) / 12.0
 payment = 0.0
-tempBalance = balance
+tempBalance = float(balance)
 
 while (True):
-    payment = round(upperBound - ((upperBound - lowerBound) / 2.0), 3)
+    payment = upperBound - ((upperBound - lowerBound) / 2.0)
     for i in range(1, 13):
         tempBalance = tempBalance - payment
         tempBalance = tempBalance + ((annualInterestRate / 12.0) * tempBalance)
-    if round(tempBalance, 2) == 0.0:
+    if round(tempBalance, 1) == 0.0:
         break
     elif tempBalance < 0:
         upperBound = payment
